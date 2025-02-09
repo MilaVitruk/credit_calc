1de7ce03fe28a7f8601b8ceb44c6367e46f95b57
MilaVitruk <lucy_95@mail.ru>
2025-02-09 16:16:48 +0300

    form css

--- creditcalc/credit/forms.py
+++ creditcalc/credit/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 from credit.models import Loan
-from django.forms import HiddenInput
+from django.forms import HiddenInput, TextInput, NumberInput, DateInput
 
 
 class LoanCreationForm(forms.ModelForm):
@@ -9,7 +9,17 @@ class LoanCreationForm(forms.ModelForm):
         model = Loan
         fields = '__all__'
         widgets = {
+            'title': TextInput(attrs={'placeholder': 'Title'}),
+            'amount': NumberInput(attrs={'placeholder': 'Amount'}),
+            'rate': NumberInput(attrs={'placeholder': 'Rate, %'}),
+            'term': DateInput(attrs={'placeholder': 'Terms, days'}),
             'num_of_payments': HiddenInput(),
             'payment_per_month': HiddenInput(),
             'total_amount': HiddenInput(),
         }
+# '1 00:00:00'
+        labels = {'title': '',
+                  'amount': '',
+                  'rate': '',
+                  'term': '',
+                  }
